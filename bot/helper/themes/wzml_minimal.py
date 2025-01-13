4510d47f97be7bd4b2d424e50843b1b298c4f244
Team AphaNetwork <160410546+alphanetworkss@users.noreply.github.com>
2025-01-13 14:44:58 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,11 +2,11 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
-    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+    ST_BN1_NAME = '✨Join Channel✨'
+    ST_BN1_URL = 'https://t.me/+tbqV3FdNKAFmYjA9'
+    ST_BN2_NAME = '⚡Discussion⚡'
+    ST_BN2_URL = 'https://t.me/+50UM6xPfO282NWFl'
+    ST_MSG = '''<i>This bot allows you to download non-DRM educational videos from various platforms and mirror them to your preferred cloud storage like Google Drive, any rclone cloud, or even send them to Telegram or DDL servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
